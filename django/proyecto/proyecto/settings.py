02a7572669bc422f9265f4d8431a79e1710ac812
vivian@marcate.com.mx-at-856993685829 <vivian@marcate.com.mx-at-856993685829>
2024-01-25 12:45:04 -0600

    staticfiles

--- django/proyecto/proyecto/settings.py
+++ django/proyecto/proyecto/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'proyecto/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

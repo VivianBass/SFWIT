2766efc7a8c0ff6900883ce37f982e708a68f507
vivian@marcate.com.mx-at-856993685829 <vivian@marcate.com.mx-at-856993685829>
2024-01-25 12:16:52 -0600

    testing

--- django/proyecto/proyecto/settings.py
+++ django/proyecto/proyecto/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'estaticos/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'proyecto/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
